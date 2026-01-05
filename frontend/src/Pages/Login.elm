2009a258c0ef9c632957ff6efd5fa824c9791bb0
ElrohirGT <elrohirgt@gmail.com>
2026-01-05 17:48:28 -0600

    feat: Added basic form logic

--- frontend/src/Pages/Login.elm
+++ frontend/src/Pages/Login.elm
@@ -3,7 +3,7 @@ module Pages.Login exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
 import Types
 
 
@@ -23,19 +23,43 @@ init _ =
 
 type Msg
     = ClickedLogin
+    | UserChanged String
+    | PasswordChanged String
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UserChanged username ->
+            let
+                user =
+                    model.user
+
+                newUser =
+                    { user | username = username }
+            in
+            ( { model | user = newUser }, Cmd.none )
+
+        PasswordChanged password ->
+            let
+                user =
+                    model.user
+
+                newUser =
+                    { user | password = password }
+            in
+            ( { model | user = newUser }, Cmd.none )
+
+        ClickedLogin ->
+            ( model, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
-view _ =
+view model =
     { title = "Login"
     , body =
-        [ input [ placeholder "Username" ] []
-        , input [ placeholder "Password" ] []
+        [ input [ placeholder "Username", onInput UserChanged, value model.user.username ] []
+        , input [ placeholder "Password", onInput PasswordChanged, value model.user.password ] []
         , button [ onClick ClickedLogin ] [ text "Login" ]
         ]
     }
